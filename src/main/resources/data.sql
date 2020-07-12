4f744a6bcc9648097a98b7581bee53b7fcb49762
605843 <Abhishek.sharma162fb2@cognizant.com>
2020-07-12 14:24:06 +0530

    updated test data, modified UI

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO CONTACTS (first_name, last_name, email, phone_number, status)
 VALUES
-('Abhishek', 'Sharma', 'abhishek@gmail.com', '8126127843', 'ACTIVE'),
-('Kissy', 'Jain', 'kissy@gmail.com', '9812345671', 'ACTIVE'),
-('Suruchi', 'Jain', 'suruchi@gmail.com', '8967543210', 'ACTIVE');
+('James', 'Smith', 'james@gmail.com', '8126127843', 'ACTIVE'),
+('Maria', 'Garcia', 'maria@yahoo.com', '9812345671', 'ACTIVE'),
+('David', 'Rodriguez', 'david@gmail.com', '8967543210', 'INACTIVE');
